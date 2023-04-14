e57e3b703e62ab51a1e35408288b7b50457389ce
Duy <duy.leduc8@gmail.com>
2023-04-14 20:11:33 +0700

    get by id, update, delete API for Post

--- src/controllers/posts.py
+++ src/controllers/posts.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Body, Depends, Path
 from sqlalchemy.orm.session import Session
 
 from src.database import get_db
@@ -17,3 +17,20 @@ def create_post(data: PostRequest, db: Session = Depends(get_db)):
 @router.get("/", response_model=PostManyResponse)
 def get_posts(data: Pagination = Depends(Pagination), db: Session = Depends(get_db)):
     return posts_service.get_posts(db, data)
+
+
+@router.get("/{id}", response_model=PostResponse)
+def get_post_by_id(id: int = Path(...), db: Session = Depends(get_db)):
+    return posts_service.get_post_by_id(db, id)
+
+
+@router.put("/{id}", response_model=PostResponse)
+def update_post(
+    id: int = Path(...), data: PostRequest = Body(...), db: Session = Depends(get_db)
+):
+    return posts_service.update_post(db, id, data)
+
+
+@router.delete("/{id}")
+def delete_post(id: int = Path(...), db: Session = Depends(get_db)):
+    return posts_service.delete_post(db, id)
--- src/services/posts.py
+++ src/services/posts.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from sqlalchemy.orm.session import Session
 
 from src.models.posts import Post
@@ -22,3 +23,47 @@ def get_posts(db: Session, data: Pagination):
     query = db.query(Post).offset(data.page).limit(data.page_size)
 
     return {"items": query.all(), "total": query.count()}
+
+
+def get_post_by_id(db: Session, id: int):
+    post = db.query(Post).filter_by(id=id).first()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found"
+        )
+
+    return post
+
+
+def update_post(db: Session, id: int, data: PostRequest):
+    post = db.query(Post).filter_by(id=id).first()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found"
+        )
+
+    post.title = data.title
+    post.published = data.published
+
+    if data.content:
+        post.content = data.content
+
+    db.commit()
+
+    return post
+
+
+def delete_post(db: Session, id: int):
+    post = db.query(Post).filter_by(id=id).first()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found"
+        )
+
+    db.delete(post)
+    db.commit()
+
+    return {"message": f"Post with id {id} has been deleted"}
